e22652153137aa39cfa2db99f47e7c33f051af4c
AnastasiaGolova <135448188+AnastasiaGolova@users.noreply.github.com>
2023-09-10 20:55:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/digital-business-card-project-react/",
-  plugins: [react()],
-});
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/digital-business-card-project-react/'
+  }
+
+  return config
+})
